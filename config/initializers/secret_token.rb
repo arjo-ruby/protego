89dc4d07847049f4ae3cffb4c4dd3ed8c676da6f
arjo-ruby <arjo.debolin@gmail.com>
2013-03-07 00:17:57 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Protego::Application.config.secret_token = '4b43c467b901b35b7f6b48bc500005db7ab4abd455fdeb4438e1b9a3cae2c360440c0ef5228dbd32bd3a99ea848ccdd21965bad4ef40fb228c0aa707216652af'
